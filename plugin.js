a04b6c37a1fad813541b8d6b13ced9572bf5c003
Peter Donker <peter@bring2mind.net>
2018-07-21 16:55:59 +0200

    Fix issue with gulp-file which wasn't being picked up by merge2 as a proper vinyl stream. Also removal of gulp-util as it is deprecated.

--- plugin.js
+++ plugin.js
@@ -1,49 +1,46 @@
-'use strict';
-var dnnModule = require('./lib/module.js'),
-    dnnSkin = require('./lib/skin.js'),
-    dnnLibrary = require('./lib/library.js'),
-    dnnPersonaBar = require('./lib/personabar.js'),
-    extend = require('extend'),
-    gutil = require('gulp-util'),
-    file = require('gulp-file');
+"use strict";
+var dnnModule = require("./lib/module.js"),
+  dnnSkin = require("./lib/skin.js"),
+  dnnLibrary = require("./lib/library.js"),
+  dnnPersonaBar = require("./lib/personabar.js"),
+  extend = require("extend"),
+  file = require("gulp-file");
 
-var PLUGIN_NAME = 'gulp-dnn-manifest';
+var PLUGIN_NAME = "gulp-dnn-manifest";
 
 module.exports = function(options, originalManifestFileName) {
-
-    var defaults = {
-        version: '01.00.00',
-        dnn: {
-            projectType: "module",
-            packagesPath: "./_Packages",
-            pathToAssemblies: './bin',
-            pathToScripts: './_Installation/SQL',
-            pathToSupplementaryFiles: './_Installation',
-            excludedPaths: ['node_modules', '_references']
-        }
+  var defaults = {
+    version: "01.00.00",
+    dnn: {
+      projectType: "module",
+      packagesPath: "./_Packages",
+      pathToAssemblies: "./bin",
+      pathToScripts: "./_Installation/SQL",
+      pathToSupplementaryFiles: "./_Installation",
+      excludedPaths: ["node_modules", "_references"]
     }
-    var config = extend(true, {}, defaults, options || {});
+  };
+  var config = extend(true, {}, defaults, options || {});
 
-    if (!config) {
-        throw new PluginError(PLUGIN_NAME, 'No configuration specified.');
-    }
+  if (!config) {
+    throw new PluginError(PLUGIN_NAME, "No configuration specified.");
+  }
 
-    var manifest = "";
-    switch (config.dnn.projectType) {
-        case "skin":
-            manifest = dnnSkin(config);
-            break;
-        case "module":
-            manifest = dnnModule(config, originalManifestFileName);
-            break;
-        case "library":
-            manifest = dnnLibrary(config, originalManifestFileName);
-            break;
-        case "personabar":
-            manifest = dnnPersonaBar(config, originalManifestFileName);
-            break;
-    }
+  var manifest = "";
+  switch (config.dnn.projectType) {
+    case "skin":
+      manifest = dnnSkin(config);
+      break;
+    case "module":
+      manifest = dnnModule(config, originalManifestFileName);
+      break;
+    case "library":
+      manifest = dnnLibrary(config, originalManifestFileName);
+      break;
+    case "personabar":
+      manifest = dnnPersonaBar(config, originalManifestFileName);
+      break;
+  }
 
-    return file("version.dnn", manifest);
-
-}
+  return file("version.dnn", manifest, { src: true });
+};
